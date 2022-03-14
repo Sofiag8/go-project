8ba79611e26dc0ce1426eb3389077c8276b49f44
Sofiag8 <sofia.garcia@bimbau.co>
2022-03-14 10:52:40 -0500

    delete .idea

--- models/User.go
+++ models/User.go
@@ -4,10 +4,6 @@ import (
 	"time"
 )
 
-// GORM prefer convention over configuration,
-// by default, GORM uses ID as primary key,
-// pluralize struct name to snake_cases as table name,
-// snake_case as column name, and uses CreatedAt, UpdatedAt to track creating/updating time
 type User struct {
 	Licenses       []License `gorm:"foreignKey:UserId"`
 	ID             uint      `gorm:"primaryKey"`
